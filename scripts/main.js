a1f2b7f854685a9409559d020a8c24fe22211c58
Jorge Nascimento <jorg3nascimento@gmail.com>
2021-04-06 12:42:57 -0300

    fix bug

--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,47 @@
-const board = document.querySelector('#board')
-const funcoes = document.querySelectorAll('[data-menu]')
+const board = document.querySelector("#board");
+const funcoes = document.querySelectorAll("[data-menu]");
 
-funcoes.forEach(funcao => funcao.addEventListener('click', getPage))
+funcoes.forEach((funcao) => funcao.addEventListener("click", getPage));
 
+function getPage() {
+  isActive(this);
 
-function getPage(){
-  isActive(this)
+  const page = this.getAttribute("title");
 
-  const page = this.getAttribute('title')
-  
-  fetch(`http://127.0.0.1:5500/pages/${page}.html`)
-   .then((res) => res.text())
-    .then((html) =>board.innerHTML = html)
+  fetch(`${window.location.href}/pages/${page}.html`)
+    .then((res) => res.text())
+    .then((html) => (board.innerHTML = html));
 }
 
-function isActive(self){
+function isActive(self) {
+  const divs = self.parentNode.querySelectorAll("div");
+  divs.forEach((e) => e.classList.remove("active"));
 
-  const divs = self.parentNode.querySelectorAll('div')
-  divs.forEach((e) => e.classList.remove('active') )
-
-  self.classList.add('active') 
+  self.classList.add("active");
 }
-
 
 //
 
-
-
-function clock(){  
-  const display = document.querySelector('.display-relogio')
+function clock() {
+  const display = document.querySelector(".display-relogio");
 
   const clock = setInterval(() => {
-  const hora = `${new Date().getHours() < 10 ? "0"+new Date().getHours() : new Date().getHours()} : ${new Date().getMinutes() < 10 ? "0"+new Date().getMinutes() : new Date().getMinutes()} : ${new Date().getSeconds() < 10 ? "0"+new Date().getSeconds() : new Date().getSeconds()}`
+    const hora = `${
+      new Date().getHours() < 10
+        ? "0" + new Date().getHours()
+        : new Date().getHours()
+    } : ${
+      new Date().getMinutes() < 10
+        ? "0" + new Date().getMinutes()
+        : new Date().getMinutes()
+    } : ${
+      new Date().getSeconds() < 10
+        ? "0" + new Date().getSeconds()
+        : new Date().getSeconds()
+    }`;
 
-  display.textContent = hora;   
-  }, 1000)
+    display.textContent = hora;
+  }, 1000);
 }
 
-clock()
-
-
+clock();
